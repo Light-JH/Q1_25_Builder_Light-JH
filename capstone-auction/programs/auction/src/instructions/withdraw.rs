d022ca96e02287b252022ff74829f7f05453de40
Light-JH <phoebehu@outlook.com>
2025-02-27 18:27:34 -0600

    Box Withdraw accounts

--- capstone-auction/programs/auction/src/instructions/withdraw.rs
+++ capstone-auction/programs/auction/src/instructions/withdraw.rs
@@ -11,12 +11,12 @@ use anchor_spl::{
 pub struct Withdraw<'info> {
     #[account(mut)]
     pub bidder: Signer<'info>,
-    pub mint_b: InterfaceAccount<'info, Mint>,
+    pub mint_b: Box<InterfaceAccount<'info, Mint>>,
     #[account(
         seeds = [b"house", auction_house.name.as_bytes()],
         bump = auction_house.bump,
     )]
-    pub auction_house: Account<'info, AuctionHouse>,
+    pub auction_house: Box<Account<'info, AuctionHouse>>,
     /// CHECK: This is unchecked, because the account may or may not exist at this point.
     pub auction: AccountInfo<'info>,
     #[account(
@@ -25,13 +25,13 @@ pub struct Withdraw<'info> {
         associated_token::mint = mint_b,
         associated_token::authority = bidder,
     )]
-    pub bidder_mint_b_ata: InterfaceAccount<'info, TokenAccount>,
+    pub bidder_mint_b_ata: Box<InterfaceAccount<'info, TokenAccount>>,
     #[account(
         mut,
         associated_token::mint = mint_b,
         associated_token::authority = bid_state,
     )]
-    pub bidder_escrow: InterfaceAccount<'info, TokenAccount>,
+    pub bidder_escrow: Box<InterfaceAccount<'info, TokenAccount>>,
     #[account(
         mut,
         close = bidder,
@@ -39,7 +39,7 @@ pub struct Withdraw<'info> {
         bump = bid_state.bump,
         constraint = bid_state.auction == auction.key()
     )]
-    pub bid_state: Account<'info, BidState>,
+    pub bid_state: Box<Account<'info, BidState>>,
     pub associated_token_program: Program<'info, AssociatedToken>,
     pub token_program: Interface<'info, TokenInterface>,
     pub system_program: Program<'info, System>,
